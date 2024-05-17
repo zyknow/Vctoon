063972fefec25631896cde5830ef28c641ddb678
zyknow <1047606598@qq.com>
2024-05-17 17:49:31 +0800

    ef core migration

--- src/Vctoon.EntityFrameworkCore/Migrations/20240514122333_Change_Entities_To_FullAudited.Designer.cs
+++ src/Vctoon.EntityFrameworkCore/Migrations/20240514122333_Change_Entities_To_FullAudited.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vctoon.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -11,9 +12,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Vctoon.Migrations
 {
     [DbContext(typeof(VctoonDbContext))]
-    partial class VctoonDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240514122333_Change_Entities_To_FullAudited")]
+    partial class Change_Entities_To_FullAudited
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
