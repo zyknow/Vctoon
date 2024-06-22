e3aab1275c37a92fadbd73ea1d3c66a900f732b3
zyknow <1047606598@qq.com>
2024-06-22 21:14:01 +0800

    add pagination component

---
+++ src/Vctoon.Blazor.Client/Components/Paginations/Page.cs
@@ -0,0 +1,27 @@
+﻿namespace Vctoon.Blazor.Client.Components.Paginations;
+
+/// <summary>
+///     Indicates a pagination behavior.
+/// </summary>
+public enum Page
+{
+    /// <summary>
+    ///     Navigate to the first page of results.
+    /// </summary>
+    First,
+
+    /// <summary>
+    ///     Navigate to the previous page of results.
+    /// </summary>
+    Previous,
+
+    /// <summary>
+    ///     Navigate to the next page of results.
+    /// </summary>
+    Next,
+
+    /// <summary>
+    ///     Navigate to the last page of results.
+    /// </summary>
+    Last
+}
