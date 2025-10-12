c0c538008a7e71d8fd9bffc3a1923b21abd39867
I"s <1047606598@qq.com>
2025-10-12 22:48:10 +0800

    feat(library): update library route to use null component for future implementation

--- vben/apps/web-ele/src/router/routes/modules/vctoon.ts
+++ vben/apps/web-ele/src/router/routes/modules/vctoon.ts
@@ -26,7 +26,7 @@ const routes: RouteRecordRaw[] = [
     },
     name: 'Library',
     path: '/library',
-    component: () => import('#/views/library/library-main.vue'),
+    component: () => null,
   },
   {
     meta: {
